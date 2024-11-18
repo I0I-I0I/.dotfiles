c086c9e667644d07bf454ee75db40e26ed9d739c
I0I-I0I <azudjndj@gmail.com>
2024-11-18 21:55:06 +0300

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,27 +2,24 @@ set -gx PATH $PATH /usr/local/bin
 set -gx PATH $PATH /usr/local/go/bin
 set -gx PATH $PATH ~/.local/bin
 set -gx PATH $PATH ~/.cargo/bin
-set -gx PATH $PATH ~/.tmux/scripts
-set -gx PATH $PATH ~/yazi
+set -gx PATH $PATH ~/apps
 
-set -gx todo /mnt/c/Users/azudj/Dropbox/TODO
+# Aliases
+alias bc="batcat"
+alias py="python3"
+alias nvim="~/nvim-linux64/bin/nvim"
+alias vim="NVIM_APPNAME=nvim-mini nvim"
 
 # Tmux
+set -gx PATH $PATH ~/.tmux/scripts
+
 alias tma="tmux attach"
 alias tmat="tmux attach -t"
 alias tmls="tmux ls"
 alias tmk="tmux kill-session"
 
-# Aliases
-alias man="tldr"
-alias bc="batcat"
-alias py="python3"
-alias prtr='prettier --write "**/*.{js,jsx,ts,tsx,html,css,json}"'
-alias nvim="~/nvim-linux64/bin/nvim"
-alias vim="NVIM_APPNAME=nvim-mini nvim"
-
-# Binds
 bind \ct "tmux-sessionizer"
+bind -k nul "tmux-yazi"
 
 # Theme
 fish_config theme choose "Rose Pine"
