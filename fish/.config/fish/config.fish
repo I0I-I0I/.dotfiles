f0199ec5c105b988b87f9a638a454d69c2c3ca04
I0I-I0I <azudjndj@gmail.com>
2024-12-15 14:24:09 +0300

    some changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,12 +3,13 @@ set -gx PATH $PATH /usr/local/go/bin
 set -gx PATH $PATH ~/.local/bin
 set -gx PATH $PATH ~/.cargo/bin
 set -gx PATH $PATH ~/apps
+set -gx PATH $PATH ~/.local/share/pnpm
 
 # Aliases
 alias bc="batcat"
 alias py="python3"
-alias nvim="~/nvim-linux64/bin/nvim"
-alias vim="NVIM_APPNAME=nvim-mini nvim"
+# alias nvim="~/nvim-linux64/bin/nvim"
+alias vim="nvim"
 
 # Tmux
 set -gx PATH $PATH ~/.tmux/scripts
@@ -17,6 +18,7 @@ alias tma="tmux attach"
 alias tmat="tmux attach -t"
 alias tmls="tmux ls"
 alias tmk="tmux kill-session"
+alias tmks="tmux kill-server"
 
 bind \ct "tmux-sessionizer"
 bind -k nul "tmux-yazi"
