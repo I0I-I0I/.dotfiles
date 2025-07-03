07a46b2372de5b7a45a3baa379251581fe288bfb
I0I-I0I <azudjndj@gmail.com>
2025-07-03 23:41:22 +0300

    fix: waybar on-click functions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,4 @@
-set -gx PATH $PATH /usr/local/bin
-set -gx PATH $PATH /usr/local/go/bin
-set -gx PATH $PATH ~/.local/bin
-set -gx PATH $PATH ~/.cargo/bin
-set -gx PATH $PATH ~/apps
 set -gx PATH $PATH ~/.local/share/pnpm
-set -gx PATH $PATH ~/apps/yazi/target/release
-set -gx PATH $PATH ~/apps/neovim/build/bin
-set -gx PATH $PATH ~/go/bin
 set -gx PATH $PATH ~/.dotfiles/scripts
 
 set -gx ELECTRON_OZONE_PLATFORM_HINT auto
@@ -18,36 +10,21 @@ set -gx MOZ_ENABLE_WAYLAND 1
 set -gx QT_QPA_PLATFORM xcb
 
 set -gx EDITOR /usr/local/bin/nvim
-
-# Aliases
-alias py="uv run python"
-alias vi="/sbin/vim"
-alias vim="CONF=text nvim"
-alias poe="uv run poe"
-alias ls="ls --color=auto"
-alias la="ls -Alhv --group-directories-first --color=auto"
-alias ll="ls -vA --group-directories-first --color=auto"
-
-# Tmux
 set -gx PATH $PATH ~/.tmux/scripts
 
-alias tm="tmux -L default"
-alias tms="tmux -L second"
-alias tmls="ls /tmp/tmux-$(id -u)"
+alias py="uv run python"
+alias vi="/sbin/vim"
+alias vim="nvim"
+alias cim="CONF=code nvim"
+alias poe="uv run poe"
+alias la="ls -Alhv --group-directories-first --color=auto"
+alias ll="ls -vA --group-directories-first --color=auto"
 alias tmk="tmux kill-session"
 alias tmks="tmux kill-server"
 
 bind \cs "tmux-sessionizer"
-bind -k nul "tmux-yazi"
 
-alias wifi="nmtui"
-alias bt="bluetui"
-alias theme="$HOME/.dotfiles/scripts/set-theme"
-alias btop="sudo btop"
-
-# Theme
 fish_config theme choose "Rose Pine"
-# ~/.local/bin/mise activate fish | source
 
 # pnpm
 set -gx PNPM_HOME "/home/i0i/.local/share/pnpm"
